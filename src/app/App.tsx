d0aa768e39bb955e639ba546a6bd3d86dca1c87a
baraasowwan1 <105966196+baraasowwan1@users.noreply.github.com>
2026-01-20 18:32:19 +0300

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
 import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
-import { HowItWorks } from "./components/HowItWorks";
-import { OurWork } from "./components/OurWork";
-import { Benefits } from "./components/Benefits";
 import { QuoteForm } from "./components/QuoteForm";
+import { OurWork } from "./components/OurWork";
+import { HowItWorks } from "./components/HowItWorks";
+import { Benefits } from "./components/Benefits";
 import { Testimonials } from "./components/Testimonials";
 import { FAQ } from "./components/FAQ";
 import { Footer } from "./components/Footer";
